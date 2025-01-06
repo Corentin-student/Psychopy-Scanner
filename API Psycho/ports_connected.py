f8b10b0c2bfaf2f2fbc75fbc7add9deda4bdb29d
Corentin-student <corentin.warzee@student.uclouvain.be>
2025-01-06 03:21:20 +0100

    Update ports_connected.py

--- API Psycho/ports_connected.py
+++ API Psycho/ports_connected.py
@@ -4,4 +4,4 @@ ports = serial.tools.list_ports.comports()
 for port, desc, hwid in sorted(ports):
     print(f"{port}: {desc} [{hwid}]")
 
-#This file is useful to know on wich serial port the device is connected
+#This file is useful to know on which serial port the device is connected
